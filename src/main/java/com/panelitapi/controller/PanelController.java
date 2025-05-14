ff952bbb527b114294b1e5090378571c05cadb3a
Vikkavv <96725063+Vikkavv@users.noreply.github.com>
2025-05-14 21:26:02 +0200

    Added the possiblity to find, delete and update a panel
    Now its possible to delete, update and find a panel by it's id.

--- src/main/java/com/panelitapi/controller/PanelController.java
+++ src/main/java/com/panelitapi/controller/PanelController.java
@@ -2,15 +2,16 @@ package com.panelitapi.controller;
 
 import com.panelitapi.model.Panel;
 import com.panelitapi.model.User;
-import com.panelitapi.service.PanelService;
-import com.panelitapi.service.UserService;
+import com.panelitapi.service.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -18,16 +19,51 @@ import java.util.List;
 public class PanelController {
     private PanelService panelService;
     private UserService userService;
+    private SessionService sessionService;
 
     @Autowired
-    public PanelController(PanelService panelService, UserService userService) {
+    public PanelController(PanelService panelService, UserService userService, SessionService sessionService) {
         this.panelService = panelService;
         this.userService = userService;
+        this.sessionService = sessionService;
     }
 
     @GetMapping("/ofUser/{nickname}")
     public ResponseEntity<List<Panel>> getPanelsOfUser(@PathVariable String nickname) {
         User user = userService.getGeneralUserInfoByNickname(nickname);
         return ResponseEntity.ok(panelService.getPanelsOfUser(user));
+    }
+
+    @GetMapping("/findById/{id}")
+    public ResponseEntity<Panel> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(panelService.findById(id));
+    }
+
+    @PutMapping("/Update")
+    public ResponseEntity<Boolean> updatePanel(@RequestBody Panel panel) {
+        return ResponseEntity.ok(panelService.updatePanel(panel));
+    }
+
+    @PostMapping(value = "/Create", consumes = "multipart/form-data")
+    public ResponseEntity<Long> createPanel(@RequestPart("name") String name, @RequestPart("additionalInfoJson") String jsonInfo, @RequestPart(name = "coverPhoto", required = false) MultipartFile coverPhoto, @RequestPart(name = "backgroundPhoto", required = false) MultipartFile backgroundPhoto, @CookieValue(value = "SESSIONID", defaultValue = "") String sessionId, HttpServletRequest request) throws IOException {
+        User user = null;
+        if(sessionId == null || sessionId.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        else user = sessionService.findById(sessionId).getUser();
+
+        return ResponseEntity.ok(panelService.createPanel(name, user, coverPhoto, backgroundPhoto, jsonInfo, request));
+    }
+
+    @DeleteMapping("/Delete")
+    @Transactional
+    public ResponseEntity<Boolean> delete(@RequestBody Panel panel, @CookieValue(value = "SESSIONID", defaultValue = "") String sessionId) {
+         User user = null;
+         if(sessionId != null || !(sessionId.isEmpty()))
+             user = sessionService.findById(sessionId).getUser();
+         if((sessionId == null || sessionId.isEmpty()) || (!panel.getCreatorId().equals(user.getId()))) {
+             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+         }
+         return ResponseEntity.ok(panelService.deletePanel(panel.getId()));
     }
 }
--- src/main/java/com/panelitapi/service/PanelService.java
+++ src/main/java/com/panelitapi/service/PanelService.java
@@ -3,10 +3,15 @@ package com.panelitapi.service;
 import com.panelitapi.model.Panel;
 import com.panelitapi.model.PanelParticipant;
 import com.panelitapi.model.User;
+import com.panelitapi.repository.NoteRepository;
 import com.panelitapi.repository.PanelRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,11 +20,19 @@ public class PanelService {
 
     private PanelRepository panelRepository;
     private PanelParticipantService panelParticipantService;
+    private NoteRepository noteRepository;
+    private FileStorageService imageStorageService;
 
     @Autowired
-    public PanelService(PanelRepository panelRepository, PanelParticipantService panelParticipantService) {
+    public PanelService(PanelRepository panelRepository, PanelParticipantService panelParticipantService, NoteRepository noteRepository ,FileStorageService fileStorageService) {
         this.panelRepository = panelRepository;
         this.panelParticipantService = panelParticipantService;
+        this.noteRepository = noteRepository;
+        this.imageStorageService = fileStorageService;
+    }
+
+    public Panel findById(Long id){
+        return panelRepository.findById(id).orElseThrow();
     }
 
     public List<Panel> getPanelsOfUser(User user) {
@@ -29,5 +42,40 @@ public class PanelService {
             panels.add(panelParticipant.getPanel());
         }
         return panels;
+    }
+
+    public Long createPanel(String name, User creator, MultipartFile coverPhoto, MultipartFile backgroundPhoto, String json, HttpServletRequest request) throws IOException {
+        Panel panel = new Panel();
+        panel.setName(name);
+        panel.setCreatorId(creator.getId());
+        panel.setCreationDate(LocalDate.now());
+        panel.setLastEditedDate(LocalDate.now());
+        panel.setIsBlocked(false);
+        panel.setAdditionalInfo(json);
+
+        if(coverPhoto != null) {
+            String imageName = imageStorageService.savePanelImage(coverPhoto);
+            panel.setCoverPhoto(imageStorageService.getPanelImgUrl(imageName,request));
+        }
+        if(backgroundPhoto != null) {
+            String imageName = imageStorageService.savePanelImage(backgroundPhoto);
+            panel.setBackgroundPhoto(imageStorageService.getPanelImgUrl(imageName,request));
+        }
+        panel = panelRepository.save(panel);
+        panelParticipantService.addPanelParticipant(creator, panel,true,true);
+        return panel.getId();
+    }
+
+    public Boolean updatePanel(Panel panel){
+        panel.setLastEditedDate(LocalDate.now());
+        panelRepository.save(panel);
+        return true;
+    }
+
+    public Boolean deletePanel(Long id){
+        panelParticipantService.deletePanelParticipantsByPanel(findById(id));
+        noteRepository.deleteAllByPanel(findById(id));
+        panelRepository.deleteById(id);
+        return true;
     }
 }
