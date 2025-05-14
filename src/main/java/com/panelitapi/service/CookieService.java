9ceb3e69cb6b9c75cb9f7d8411a7215f24c418f3
Vikkavv <96725063+Vikkavv@users.noreply.github.com>
2025-05-14 21:24:33 +0200

    Added "Remember me" function in cookie service
    Added the functionality of "Remember me" that alows users to mantain theirs sessions open for 10 days.

--- src/main/java/com/panelitapi/service/CookieService.java
+++ src/main/java/com/panelitapi/service/CookieService.java
@@ -8,12 +8,19 @@ import java.util.UUID;
 @Service
 public class CookieService {
 
-    public Cookie createSessionCookie(){
+    public Cookie createSessionCookie(Boolean rememberMe){
         Cookie cookie = new Cookie("SESSIONID", UUID.randomUUID().toString());
         cookie.setHttpOnly(true);
         cookie.setSecure(/*true*/ false);
         cookie.setPath("/");
+        if(rememberMe){
+            cookie.setMaxAge(10/*Days*/ * 24/*Hours*/ * 60/*Minutes*/ * 60/*Seconds*/);
+        }
         return cookie;
+    }
+
+    public Cookie createSessionCookie(){
+        return createSessionCookie(false);
     }
 
     public Cookie deleteSession(Cookie cookie){
